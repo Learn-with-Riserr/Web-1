189325eb7b4909f87c85fd9aa353578d1fb3d2d8
Riserr <cho.steven14@gmail.com>
2022-11-28 21:50:53 +0700

    Push to GitHub

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// kode JS
+function clickHome () {
+    alert ("Ini Halaman Home");
+}
+function clickCourse () {
+    alert ("Ini Halaman Course");
+}
+function clickBlog () {
+    alert ("Ini Halaman Blog");
+}
